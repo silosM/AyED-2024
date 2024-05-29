d4f1389eae2d359c8ee8855995ab5e59eef45f69
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 14:47:20 -0300

    Add files via upload

---
+++ Etapa 1/3_Solis_DimensionesDeEsferaCorrecoin/3_Solis_DimensionesDeEsferaCorrecoin/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Solis_DimensionesDeEsferaCorrecoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, radio, superficie, volumen;
+            pi = 3.14;
+            Console.WriteLine("decime el radio de la esfera:");
+            radio = int.Parse(Console.ReadLine());
+            superficie = 4 * pi * radio * radio;
+            volumen = ((pi * 4)/3 * (radio*radio*radio));
+
+            Console.WriteLine("superficie =" + superficie);
+            Console.WriteLine("volumen =" + volumen);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
