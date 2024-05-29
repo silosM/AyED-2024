fbf3a6d4e2a6464a53bbcd6676bfc1096d711421
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 16:35:37 -0300

    Add files via upload

---
+++ Etapa 1/19_Solis_HastaN/19_Solis_HastaN/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Solis_HastaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, N;
+            num = 0;
+            Console.WriteLine("Ingresa un numero");
+            N = int.Parse(Console.ReadLine());
+            while (num<=N)
+            {
+                if (num % 2==0)
+                {
+                    Console.WriteLine(num);
+                }
+
+                num++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
