ea6057e2a3fd2dd1bd2ecbbe01ab62e8aae4f672
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 16:50:38 -0300

    Add files via upload

---
+++ Etapa 2/2_Solis_ElRayoCarreraVeloz/2_Solis_ElRayoCarreraVeloz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Solis_ElRayoCarreraVeloz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("¿Cuantas vueltas va a dar el Rayo Macqueen?");
+            int vueltas = int.Parse(Console.ReadLine());
+            
+
+            int[] Eltiempo = new int[vueltas];
+            int record = 1000000000;
+            int tiempo = 0;
+            for (int i = 0; i < vueltas; i++)
+            {
+                Console.WriteLine("Cuantos segundos tardo en dar la vuelta N°" + (i + 1));
+                Eltiempo[i] = int.Parse(Console.ReadLine());
+
+                if (Eltiempo[i] < record)
+                {
+                    record = Eltiempo[i];
+                }
+                tiempo = tiempo + Eltiempo[i];
+            }
+            Console.WriteLine("Tiempo total: "+tiempo+" segundos");
+            Console.WriteLine("Promedio del tiempo de las vueltas: "+(tiempo/vueltas)+" segundos");
+            Console.WriteLine("La mejor vuelta: " + record+ " segundos");
+
+
+            Console.ReadKey();
+        }
+    }
+}
