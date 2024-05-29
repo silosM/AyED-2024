8f473bf82e2839876626ff7e7db183f88bbc6082
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 15:03:08 -0300

    Add files via upload

---
+++ Etapa 1/7_Solis_AlReves/7_Solis_AlReves/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Solis_AlReves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string l1, l2, l3;
+
+            Console.WriteLine("elige 3 letras:");
+            l1 = Console.ReadLine();
+            l2 = Console.ReadLine();
+            l3 = Console.ReadLine();
+
+
+            Console.WriteLine("Aqui tienes :D");
+            Console.WriteLine(l1);
+            Console.WriteLine(l2);
+            Console.WriteLine(l3);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
