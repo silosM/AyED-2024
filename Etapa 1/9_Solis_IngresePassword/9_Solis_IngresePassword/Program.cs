ea3e254c50a9e13d85fb5e9e4f1521f877291bf9
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 15:08:53 -0300

    Add files via upload

---
+++ Etapa 1/9_Solis_IngresePassword/9_Solis_IngresePassword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Solis_IngresePassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contraseña, inten;
+            contraseña = "el pepo";
+
+
+            Console.WriteLine("cual es la contra?");
+            inten = Console.ReadLine();
+
+            if (contraseña == inten)
+            {
+                Console.WriteLine("Correcto");
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
