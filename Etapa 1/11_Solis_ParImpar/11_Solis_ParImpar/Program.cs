f6c983eb254f6da630933915c9f7061d1a3e75bc
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 15:15:49 -0300

    Add files via upload

---
+++ Etapa 1/11_Solis_ParImpar/11_Solis_ParImpar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Solis_ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero, resto;
+
+            Console.WriteLine("dime un numero entero y te dire si es par o inpar");
+            entero = int.Parse(Console.ReadLine());
+
+            resto = entero % 2;
+
+            if (resto == 0)
+            {
+                Console.WriteLine(" tu numero es par");
+            }
+            else
+            {
+                Console.WriteLine("tu numero es impar");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
