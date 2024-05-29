290e1585f08686bc37f0933d123ee14724131844
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 15:58:48 -0300

    Add files via upload

---
+++ Etapa 1/13_Solis_DiezVeces/13_Solis_DiezVeces/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Solis_DiezVeces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            int contador;
+            contador = 0;
+            Console.WriteLine("dame un palabra y te la dire 10 veces");
+            palabra = Console.ReadLine();
+
+            Console.WriteLine("aqui tienes");
+
+
+            while (contador < 10)
+            {
+             Console.WriteLine(palabra);
+             contador++;
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
