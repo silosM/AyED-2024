14b1c1db9b52a408cdd5c497c24c5467e8a97f17
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 15:07:13 -0300

    Add files via upload

---
+++ Etapa 1/8_Solis_EsMayor/8_Solis_EsMayor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Solis_EsMayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            Console.WriteLine("Cuantos años tienes?");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine("eres mayor de edad");
+            }
+            else if (edad < 18)
+            {
+                Console.WriteLine("eres menor de edad");
+            }
+            else
+            {
+                Console.WriteLine("tienes 18");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
