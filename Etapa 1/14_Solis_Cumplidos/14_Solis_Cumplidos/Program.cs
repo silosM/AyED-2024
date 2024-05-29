2a07d6482ff0367b622bcd8dd1a072431ca38e61
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 16:00:58 -0300

    Add files via upload

---
+++ Etapa 1/14_Solis_Cumplidos/14_Solis_Cumplidos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Solis_Cumplidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad, contador;
+
+            Console.WriteLine("Cuantos años tienes?");
+            edad = int.Parse(Console.ReadLine());
+
+            contador = 1;
+
+            while (contador <= edad)
+            {
+                Console.WriteLine("cumpliste " + contador);
+                contador++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
