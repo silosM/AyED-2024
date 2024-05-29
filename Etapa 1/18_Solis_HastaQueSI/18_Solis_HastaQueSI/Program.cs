5ff99b54c7a4ea80f569578c38b4ab85f21b07dd
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 16:32:21 -0300

    Add files via upload

---
+++ Etapa 1/18_Solis_HastaQueSI/18_Solis_HastaQueSI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Solis_HastaQueSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string si;
+
+            Console.WriteLine("DIga SI para terminar el programa");
+            si = Console.ReadLine();
+
+            while (si != "SI")
+            {
+                Console.WriteLine("¿Desea terminar el programa?");
+                si = Console.ReadLine();
+            }
+            if (si == "SI")
+            {
+                Console.WriteLine("Hasta la vista baby");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
