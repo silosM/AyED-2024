3c340af968f71226559386f3e5f8ed7c4e61da7c
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 16:07:38 -0300

    Add files via upload

---
+++ Etapa 1/15_Solis_CuentaRegresiva/15_Solis_CuentaRegresiva/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Solis_CuentaRegresiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            
+
+            Console.WriteLine("escribi un numero entero y positivo:");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+            Console.WriteLine("te muestro la cuenta regresiva");
+            while (num>=0)
+            {
+                Console.WriteLine(num);
+                num--;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 1/16_Solis_FizzBuzz/16_Solis_FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Solis_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = 0;
+
+            while (num <= 100)
+            {
+                if (num % 5 == 0 && num % 3 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+                num++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
