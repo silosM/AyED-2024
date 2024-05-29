7264744a915036b04cb36a82267b3bc0eeafecdd
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 14:32:31 -0300

    Add files via upload

---
+++ Etapa 0/1_Solis_HolaNombre/1_Solis_HolaNombre/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Solis_HolaNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Nombre;
+            Console.WriteLine("Como te llamas?");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + Nombre);
+            Console.ReadKey();
+        }
+    }
+}
