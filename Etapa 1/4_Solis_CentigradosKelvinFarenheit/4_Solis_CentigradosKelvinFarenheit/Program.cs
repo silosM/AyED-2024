8323ffab23dd099134af95de75b82f6b66f99b0a
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 14:48:23 -0300

    Add files via upload

---
+++ Etapa 1/4_Solis_CentigradosKelvinFarenheit/4_Solis_CentigradosKelvinFarenheit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Solis_CentigradosKelvinFarenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cel, fahren, kel;
+            Console.WriteLine("cuantos grados hace?, lo pasare a kelvin y fahrenheit");
+
+            cel = int.Parse(Console.ReadLine());
+
+            kel = 273 + cel;
+            fahren = cel * 18 / 10 + 32;
+            Console.WriteLine("kelvin = " + kel);
+            Console.WriteLine("fahrenheit = " + fahren);
+
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
