be7e261093dca11c5a09072ac096993b0492e3bd
silosM <167358874+silosM@users.noreply.github.com>
2024-05-29 16:29:24 -0300

    Add files via upload

---
+++ Etapa 1/17_Solis_MiPrimerMenuObjetivo/17_Solis_MiPrimerMenuObjetivo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Solis_MiPrimerMenuObjetivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion;
+
+            Console.WriteLine("Bienvenido/a, tenga el menu de hoy, eliga un numero de las opciones");
+            Console.WriteLine("Opcion 1");
+            Console.WriteLine("Opcion 2");
+            Console.WriteLine("Opcion 3");
+
+            opcion = Console.ReadLine();
+
+            Console.Clear();
+
+            if (opcion == "1")
+            {
+                Console.WriteLine("Es una salchipapa, elegiste bien");
+            }
+            else if (opcion == "2")
+            {
+                Console.WriteLine("Buena elección, ahora le traigo su amburguesa");
+            }
+            else if (opcion == "3")
+            {
+                Console.WriteLine("Le toco un silpancho, fue una buena elección");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
