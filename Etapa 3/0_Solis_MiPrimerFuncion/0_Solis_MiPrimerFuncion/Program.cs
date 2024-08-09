8c9989d5b53e143b14e668c89a8f6de7e0f29014
silosM <167358874+silosM@users.noreply.github.com>
2024-08-09 16:22:28 -0300

    Add files via upload

---
+++ Etapa 3/0_Solis_MiPrimerFuncion/0_Solis_MiPrimerFuncion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string saludar(string nombre)
+        {
+            string saludo;
+            return saludo = ("Hola "+nombre);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, Como te llamas?");
+            string usuario = Console.ReadLine();
+            Console.WriteLine(saludar(usuario));
+
+            Console.ReadKey();
+        }
+    }
+}
