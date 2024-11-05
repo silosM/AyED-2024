a47ba7b0aec341e4b11c91f98156d8b82375afa7
silosM <167358874+silosM@users.noreply.github.com>
2024-11-05 16:34:55 -0300

    Add files via upload

---
+++ Etapa 1/1_Solis_LaSumaAnterior/1_Solis_LaSumaAnterior/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Solis_LaSumaAnterior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, SumaTotal;
+
+            num1 = 18;
+            num2 = 2;
+            num3 = 10;
+
+            SumaTotal = num1 + num2 + num3;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+            Console.WriteLine(num1 + "+" + num2 + "+" + num3 + "=" + SumaTotal);
+
+            Console.ReadKey(); 
+
+        }
+    }
+}
